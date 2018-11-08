96a1708f1cd31c3aee1c61b96c1b288c1b135739
ahmed <bayt@hotmail.de>
2018-11-08 17:21:58 +0100

    ''

--- src/app/apps/fo/fo.component.ts
+++ src/app/apps/fo/fo.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-fo',
   templateUrl: './fo.component.html',
-  styles: []
+  styles: [
+    `.collapse-button {
+      background: url("./assets/img/arrow-circle-top-3x.png") no-repeat bottom;
+      background-size: 100%;
+    }`
+  ]
 })
 export class FoComponent implements OnInit {
 
+  collapsed = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  switchImage() {
+    if (!this.collapsed) {
+     document.getElementById('collapse-btn').style.backgroundImage = 'url("./assets/img/arrow-circle-bottom-3x.png")';
+    } else {
+      document.getElementById('collapse-btn').style.backgroundImage = 'url("./assets/img/arrow-circle-top-3x.png")';
+    }
+    this.collapsed = !this.collapsed;
+  }
+
+  switchCollapse() {
+    document.getElementById('collapse-btn').click();
+  }
 }
